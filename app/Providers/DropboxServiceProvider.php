8b085800dacd833be6cb0aedd7692a9d44f8a315
Jnyawach <60853597+Jnyawach@users.noreply.github.com>
2022-05-12 17:08:37 +0300

    try

--- app/Providers/DropboxServiceProvider.php
+++ app/Providers/DropboxServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use League\Flysystem\Filesystem;
-use Spatie\Dropbox\Client as DropboxClient;
+use Spatie\Dropbox\Client;
 use Spatie\FlysystemDropbox\DropboxAdapter;
 use Storage;
 
@@ -18,13 +18,11 @@ class DropboxServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Storage::extend('dropbox', function ($app, $config) {
-            $client = new DropboxClient(
-                $config['authorization_token']
-            );
+        $client = new Client($config['authorization_token']);
 
-            return new Filesystem(new DropboxAdapter($client));
-        });
+      $adapter = new DropboxAdapter($client);
+
+      $filesystem = new Filesystem($adapter, ['case_sensitive' => false]);
     }
 
     /**
