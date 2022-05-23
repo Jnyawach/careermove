50f31c0c869f046aa00b2e7ad2d7c375b1a9b2da
Jnyawach <60853597+Jnyawach@users.noreply.github.com>
2022-05-23 14:25:24 +0300

    index

--- app/Console/Commands/SiteIndex.php
+++ app/Console/Commands/SiteIndex.php
@@ -6,8 +6,7 @@ use App\Models\Job;
 use Illuminate\Console\Command;
 use Google_Client;
 use Google_Service_Indexing;
-use Google_Http_Batch;
-use GuzzleHttp\Psr7\Request as CreateRequest;
+use Google_Service_Indexing_UrlNotification;
 
 
 class SiteIndex extends Command
@@ -35,60 +34,42 @@ class SiteIndex extends Command
     {
         $jobs=Job::where('status_id',2)->limit(100)->get();
         if($jobs->count()>0){
-            $url=$jobs->pluck('slug');
-
-
-
+            $urls=$jobs->pluck('slug');
 
         require_once 'public/google-api-php-client/vendor/autoload.php';
 
 
-        try {
-            $client = new \Google_Client();
+        $googleClient = new Google_Client();
 
-                $client->setAuthConfig('storage/app/google_auth_config.json');
-                $client->addScope('https://www.googleapis.com/auth/indexing');
-                $client->setUseBatch(true);
+        // Add here location to the JSON key file that you created and downloaded earlier.
+        $googleClient->setAuthConfig( 'storage/app/google_auth_config.json' );
+        $googleClient->setScopes( Google_Service_Indexing::INDEXING );
+        $googleClient->setUseBatch( true );
 
-                $service = new \Google_Service_Indexing($client);
-                $batch = $service->createBatch();
+        $service = new Google_Service_Indexing( $googleClient );
+        $batch = $service->createBatch();
 
-                // add request
-                $postBody = new \Google_Service_Indexing_UrlNotification();
-                $postBody->setType('URL_UPDATED');
-                $postBody->setUrl('https://careermove.co.ke/listings/' . $url);
+        $postBody = new Google_Service_Indexing_UrlNotification();
 
-
-                //$batch->add($service->urlNotifications->publish($postBody));
-                // ---- add request
-
-                //$results = $batch->execute(); // it does authorize in execute()
-                $apiKey='2026dea745db4043a7749d299cc07e23';
-                $body='https://careermove.co.ke/listings/' . $url;
-                $http = \Http::post(
-                    "https://www.bing.com/webmaster/api.svc/json/SubmitUrlbatch?apikey=" . $apiKey,
-                    [
-                        "siteUrl" => 'https://careermove.co.ke',
-                        "urlList" =>$body,
-
-                    ]
-                );
-                dd($http->body());
-
-                foreach ($jobs as $job){
-                    $job->update([
-                        'index_status'=>1,
-
-                    ]);
-                }
+        // Use URL_UPDATED for new or updated pages.
+        // Use URL_DELETED for deleted pages.
 
 
+        foreach($urls as $url)
+        {
+            $url='https://careermove.co.ke/listings/'.$url;
 
-          }
-          catch (\Exception $e) {
-            echo 'Caught exception: ',  $e->getMessage(), "\n";
-          }
+          $postBody->setUrl( $url );
+          $postBody->setType('URL_UPDATED');
+          $batch->add( $service->urlNotifications->publish( $postBody ) );
         }
 
+        $results = $batch->execute();
+        dd($results);
+
+
+
+
     }
+}
 }
