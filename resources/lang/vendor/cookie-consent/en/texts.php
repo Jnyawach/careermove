dcab17a3d32358a38b73491386fce7a7b3297cd8
Jnyawach <60853597+Jnyawach@users.noreply.github.com>
2022-05-24 16:25:00 +0300

    cookie-policy

---
+++ resources/lang/vendor/cookie-consent/en/texts.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'message' => 'Cookies help us deliver the best experience on our website. By using our website, you agree to the use of cookies.',
+    'agree' => 'ACCEPT',
+];
+?>
