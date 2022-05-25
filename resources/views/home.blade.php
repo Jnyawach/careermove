03a104133ebcf448574a4a715e530e9ca325e937
Jnyawach <60853597+Jnyawach@users.noreply.github.com>
2022-05-25 14:02:08 +0300

    cookie-policy

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -387,3 +387,21 @@
         </section>
 
 @endsection
+<div class="bottom-0 position-fixed p-2 w-100 js-cookie-consent cookie-consent">
+    <div class="cookie p-1 ps-3 rounded">
+        <div class="p-2 row">
+            <div class="row">
+                <div class="col-12 col-md-8">
+                    <p class="ml-3 cookie-consent__message">
+                        {!! trans('cookie-consent::texts.message') !!}
+                    </p>
+                </div>
+                <div class="col-12 col-md-4 text-end align-self-center">
+                    <button class="js-cookie-consent-agree cookie-consent__agree btn btn-cookie ">
+                        {{ trans('cookie-consent::texts.agree') }}
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/vendor/cookie-consent/dialogContents.blade.php
+++ resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -1,18 +1,18 @@
-<div class="bottom-0 position-fixed p-2 w-100 js-cookie-consent cookie-consent">
-    <div class="cookie p-1 ps-3 rounded">
-        <div class="p-2 row">
+<div class="js-cookie-consent cookie-consent bottom-0 position-fixed p-2 w-100">
+    <div class="cookie p-3 ps-3 rounded">
+
             <div class="row">
-                <div class="col-12 col-md-8">
+                <div class="col-12 col-md-10">
                     <p class="ml-3 cookie-consent__message">
                         {!! trans('cookie-consent::texts.message') !!}
                     </p>
                 </div>
-                <div class="col-12 col-md-4 text-end align-self-center">
-                    <button class="js-cookie-consent-agree cookie-consent__agree btn btn-cookie ">
+                <div class="col-12 col-md-2 align-self-center">
+                    <button class="js-cookie-consent-agree cookie-consent__agree btn btn-cookie">
                         {{ trans('cookie-consent::texts.agree') }}
                     </button>
                 </div>
             </div>
-        </div>
+
     </div>
 </div>
