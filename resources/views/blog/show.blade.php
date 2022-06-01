ec91e4f9f570bcbf68a4389526667af7cb06117d
Jnyawach <60853597+Jnyawach@users.noreply.github.com>
2022-06-01 19:04:44 +0300

    less-image

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -13,11 +13,7 @@
             "@id": "https://www.careermove.co.ke/blog/{{$post->slug}}"
           },
           "headline": "{{$post->title}}",
-          "image": [
-
-            "\{{$post->getFirstMediaUrl('imageCard')}}\",
-
-           ],
+          
           "datePublished": "{{$post->created_at->todatestring()}}",
           "dateModified": "{{$post->updated_at->todatestring()}}",
           "author":{
