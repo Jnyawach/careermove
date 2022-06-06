34710844939501326e398e6a0845cc7976976273
Jnyawach <60853597+Jnyawach@users.noreply.github.com>
2022-06-06 12:38:20 +0300

    added-ads

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -124,6 +124,17 @@
                             (adsbygoogle = window.adsbygoogle || []).push({});
                         </script>
                     </div>
+                    <div class="mt-3 mb-3">
+                        <script async
+                            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1649231050054855"
+                            crossorigin="anonymous"></script>
+                        <!-- horizontal-add -->
+                        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1649231050054855"
+                            data-ad-slot="4790585617" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                        <script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
+                    </div>
 
                 </div>
             </div>
