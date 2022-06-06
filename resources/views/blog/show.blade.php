dfc7d457959ebba239ab696b0b19a24d213e8bb2
Jnyawach <60853597+Jnyawach@users.noreply.github.com>
2022-06-06 18:43:17 +0300

    blog

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -21,6 +21,11 @@
             "@id": "https://www.careermove.co.ke/blog/{{$post->slug}}"
           },
           "headline": "{{$post->title}}",
+        "image": [
+        "{!!$post->getFirstMediaUrl('imageCard')!!}",
+
+       
+        ],
 
           "datePublished": "{{$post->created_at->todatestring()}}",
           "dateModified": "{{$post->updated_at->todatestring()}}",
