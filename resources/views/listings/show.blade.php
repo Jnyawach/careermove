dd0b6dfeef610a24c130da32c29968a568c34a94
Jnyawach <60853597+Jnyawach@users.noreply.github.com>
2022-06-07 16:05:24 +0300

    responsive-ad

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -87,16 +87,7 @@
                     (adsbygoogle = window.adsbygoogle || []).push({});
                 </script>
             </div>
-           <div class="advert">
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1649231050054855"
-                    crossorigin="anonymous"></script>
-                <!-- general-responsive -->
-                <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1649231050054855" data-ad-slot="4592953100"
-                    data-ad-format="auto" data-full-width-responsive="true"></ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
-            </div>
+
         </div>
     </div>
 
