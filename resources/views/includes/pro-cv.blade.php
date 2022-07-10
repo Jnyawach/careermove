3fc2ce243e10aee20c7adc3747706769b332c912
Jnyawach <nyawach41@gmail.com>
2022-07-10 17:03:10 +0300

    banner

--- resources/views/includes/pro-cv.blade.php
+++ resources/views/includes/pro-cv.blade.php
@@ -2,7 +2,7 @@
     <section class=" p-3">
         <div class="row pro-cv">
             <div class="col-12 p-3">
-                <h2 class="d-inline-block ms-2">Let a professional review na write your CV</h2>
+                <h2 class="d-inline-block ms-2">Let a professional review na write your CV today</h2>
                 <a href="{{route('services.index')}}" title="Professional Cv Writing service in Kenya"
                 class="d-inline-block float-md-end btn btn-primary me-3">
                 Order Now
