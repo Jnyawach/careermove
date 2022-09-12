ad1cab01121f374de1336b51ec7d3d3192a5f701
jnyawach <nyawach41@gmail.com>
2022-09-12 21:07:53 +0300

    feeds

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 use Laravel\Scout\Searchable;
 
 
-class Job extends Model implements
+class Job extends Model
 {
     use HasFactory, Sluggable, SluggableScopeHelpers, Searchable;
     protected $fillable=['status_id','title','industry_id','meta_description',
